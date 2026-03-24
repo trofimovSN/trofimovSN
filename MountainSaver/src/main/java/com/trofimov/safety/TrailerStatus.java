a9e35c3d719eb3535770012cf43d2ad92fb31b4b
Sergei Trofimov <trofimovsn84@gmail.com>
2026-03-24 18:54:56 -0400

    Delete MountainSaver directory

--- MountainSaver/src/main/java/com/trofimov/safety/TrailerStatus.java
+++
@@ -1,7 +0,0 @@
-package com.trofimov.safety;
-
-public class TrailerStatus {
-    public boolean isMudflapsPresent = true;
-    public double currentTreadDepth = 5.5;
-    public boolean isLampsWorking = true;
-}
--- MountainSaver/src/main/java/com/trofimov/safety/TruckStatus.java
+++
@@ -1,8 +0,0 @@
-package com.trofimov.safety;
-
-public class TruckStatus {
-    public int airPressure = 110;
-    public double steeringTread = 6.0; 
-    public double driveTread = 5.0;
-
-}
